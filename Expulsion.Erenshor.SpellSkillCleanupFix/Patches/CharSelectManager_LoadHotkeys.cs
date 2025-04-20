a99903e03a24ebcfbf96df0e68755093b7ad3cc9
Andrew Harris <31739342+iExpulsion@users.noreply.github.com>
2025-04-20 15:16:54 -0400

    Initial Commit

---
+++ Expulsion.Erenshor.SpellSkillCleanupFix/Patches/CharSelectManager_LoadHotkeys.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+
+namespace Expulsion.Erenshor.SpellSkillCleanupFix.Patches
+{
+    [HarmonyPatch(typeof(CharSelectManager), "LoadHotkeys")]
+    public class CharSelectManager_LoadHotkeys
+    {
+        public static void Prefix(CharSelectManager __instance)
+        {
+            for (var i = 0; i < GameData.CurrentCharacterSlot.HKSpells.Length; i++)
+            {
+                var spellId = GameData.CurrentCharacterSlot.HKSpells[i];
+
+                if (string.IsNullOrEmpty(spellId)) continue;
+                if (GameData.SpellDatabase.GetSpellByID(spellId) != null) continue;
+
+                GameData.CurrentCharacterSlot.HKSpells[i] = "";
+                Plugin.Log?.LogInfo($"SpellID: {spellId} not found, removing from hotkeys");
+            }
+
+            for (var i = 0; i < GameData.CurrentCharacterSlot.HKSkills.Length; i++)
+            {
+                var skillId = GameData.CurrentCharacterSlot.HKSkills[i];
+
+                if (string.IsNullOrEmpty(skillId)) continue;
+                if (GameData.SkillDatabase.GetSkillByID(skillId) != null) continue;
+
+                GameData.CurrentCharacterSlot.HKSkills[i] = "";
+                Plugin.Log?.LogInfo($"SkillID: {skillId} not found, removing from hotkeys");
+            }
+        }
+    }
+}
---
+++ Expulsion.Erenshor.SpellSkillCleanupFix/Patches/CharSelectManager_LoadSpellsAndSkills.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using HarmonyLib;
+
+namespace Expulsion.Erenshor.SpellSkillCleanupFix.Patches
+{
+    [HarmonyPatch(typeof(CharSelectManager), "LoadSpellsAndSkills")]
+    public class CharSelectManager_LoadSpellsAndSkills
+    {
+        public static void Prefix(CharSelectManager __instance)
+        {
+            var spellsToRemove = GameData.CurrentCharacterSlot.CharacterSpells
+                .Where(spell => GameData.SpellDatabase.GetSpellByID(spell) == null)
+                .ToList();
+
+            foreach (var spell in spellsToRemove)
+            {
+                GameData.CurrentCharacterSlot.CharacterSpells.Remove(spell);
+                Plugin.Log?.LogInfo($"SpellID: {spell} not found, removing from save");
+            }
+
+            var skillsToRemove = GameData.CurrentCharacterSlot.CharacterSkills
+                .Where(skill => GameData.SkillDatabase.GetSkillByID(skill) == null)
+                .ToList();
+
+            foreach (var skill in skillsToRemove)
+            {
+                GameData.CurrentCharacterSlot.CharacterSkills.Remove(skill);
+                Plugin.Log?.LogInfo($"SkillID: {skill} not found, removing from save");
+            }
+        }
+    }
+}
